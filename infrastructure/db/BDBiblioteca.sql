5b719f94f3e62c044f9c30c32cf8c19e8e49cc60
lupxg <gerson.chachagua80@gmail.com>
2026-06-12 23:46:27 -0400

    feat: add database intialization in db container
    Add .sql script for database initialization

---
+++ infrastructure/db/BDBiblioteca.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS biblioteca;
+USE biblioteca;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    rol ENUM('admin', 'bibliotecario', 'lector') NOT NULL
+);
+
+CREATE TABLE libros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    categoria VARCHAR(100),
+    stock INT NOT NULL DEFAULT 0,
+    disponible INT NOT NULL DEFAULT 0,
+    imagen_url VARCHAR(255) NULL
+);
+
+CREATE TABLE prestamos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    libro_id INT,
+    fecha_prestamo DATE NOT NULL,
+    fecha_devolucion DATE,
+    estado ENUM('prestado', 'devuelto') DEFAULT 'prestado',
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    FOREIGN KEY (libro_id) REFERENCES libros(id) ON DELETE CASCADE
+);
+
+CREATE TABLE multas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prestamo_id INT,
+    monto DECIMAL(10,2) NOT NULL,
+    pagada BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (prestamo_id) REFERENCES prestamos(id) ON DELETE CASCADE
+);
